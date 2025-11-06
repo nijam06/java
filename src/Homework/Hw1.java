f1657dc699dd5b1023c902f643f61b7f799bb710
nijam06 <nijam1197>
2025-11-06 12:42:44 +0530

    Home Work

---
+++ src/Homework/Hw1.java
@@ -0,0 +1,13 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class Hw1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.next();
+        String s2 = s.next();
+        System.out.println(s1 + " Technologies " + s2);
+
+    }
+}
---
+++ src/Homework/Hw2.java
@@ -0,0 +1,12 @@
+package Homework;
+
+
+import java.util.Scanner;
+public class Hw2 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.next();
+        System.out.println("Welcome "+s1);
+
+    }
+}
